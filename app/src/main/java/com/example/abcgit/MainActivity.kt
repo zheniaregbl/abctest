6cebcb1eb303a0baef740d6bff99168244d9e98a
zhenia_regbl <zhenieregbl@gmail.com>
2023-05-04 23:29:56 +0300

    three commit

--- app/src/main/java/com/example/abcgit/MainActivity.kt
+++ app/src/main/java/com/example/abcgit/MainActivity.kt
@@ -6,8 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val text = "text"
+        val text = "123"
     }
 }
